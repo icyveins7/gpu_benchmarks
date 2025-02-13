15a16b9354ddca8bcee4f2e78699cba3699c732e
icyveins7 <icyveins@hotmail.com>
2025-02-13 17:00:51 +0800

    add new thrust rng/transformation functors up to cplxRng w/ mul

--- proj_rng/main.cu
+++ proj_rng/main.cu
@@ -1,44 +1,8 @@
+#include "thrust_extensions.h"
 #include <iostream>
 #include <thrust/device_vector.h>
 #include <thrust/host_vector.h>
-#include <thrust/random.h>
-
-// The following is adapted from
-// cuda-samples/Samples/3_CUDA_Features/cdpQuadtree/cdpQuadtree.cu
-struct Random_generator {
-
-  int count;
-  __host__ __device__ Random_generator() : count(0) {}
-  __host__ __device__ unsigned int hash(unsigned int a) {
-    a = (a + 0x7ed55d16) + (a << 12);
-    a = (a ^ 0xc761c23c) ^ (a >> 19);
-    a = (a + 0x165667b1) + (a << 5);
-    a = (a + 0xd3a2646c) ^ (a << 9);
-    a = (a + 0xfd7046c5) + (a << 3);
-    a = (a ^ 0xb55a4f09) ^ (a >> 16);
-    return a;
-  }
-
-  __host__ __device__ __forceinline__ thrust::tuple<float, float> operator()() {
-
-    // #ifdef __CUDA_ARCH__
-    unsigned seed = hash(blockIdx.x * blockDim.x + threadIdx.x + count);
-    // thrust::generate may call operator() more than once per thread.
-    // Hence, increment count by grid size to ensure uniqueness of seed
-    count += blockDim.x * gridDim.x;
-
-    // #else
-    //
-    //     unsigned seed = hash(0);
-    //
-    // #endif
-
-    thrust::random::default_random_engine rng(seed);
-    thrust::random::uniform_real_distribution<float> distrib;
-
-    return thrust::make_tuple(distrib(rng), distrib(rng));
-  }
-};
+#include <thrust/sequence.h>
 
 int main(int argc, char *argv[]) {
   printf("Thrust RNGs\n");
@@ -48,27 +12,93 @@ int main(int argc, char *argv[]) {
     numPts = strtol(argv[1], nullptr, 10);
   printf("Using numPts = %ld\n", numPts);
 
-  int repeats = 3;
+  int repeats = 5;
   if (argc >= 3)
     repeats = strtol(argv[2], nullptr, 10);
   printf("Using repeats = %d\n", repeats);
 
-  thrust::device_vector<float> d_x(numPts);
-  thrust::device_vector<float> d_y(numPts);
+  // Test 2D RNG from example
+  {
+    printf("2D RNG\n");
+    thrust::device_vector<float> d_x(numPts);
+    thrust::device_vector<float> d_y(numPts);
+    Random_generator2d rnd;
+    for (int i = 0; i < repeats; ++i) {
+      thrust::generate(
+          thrust::make_zip_iterator(
+              thrust::make_tuple(d_x.begin(), d_y.begin())),
+          thrust::make_zip_iterator(thrust::make_tuple(d_x.end(), d_y.end())),
+          rnd);
+    }
+    thrust::host_vector<float> h_x = d_x;
+    thrust::host_vector<float> h_y = d_y;
 
-  Random_generator rnd;
-  for (int i = 0; i < repeats; ++i) {
-    thrust::generate(
-        thrust::make_zip_iterator(thrust::make_tuple(d_x.begin(), d_y.begin())),
-        thrust::make_zip_iterator(thrust::make_tuple(d_x.end(), d_y.end())),
-        rnd);
+    for (int i = 0; i < 10; ++i) {
+      printf("%.3f, %.3f\n", h_x[i], h_y[i]);
+    }
   }
-  thrust::host_vector<float> h_x = d_x;
-  thrust::host_vector<float> h_y = d_y;
+  printf("====================================\n");
 
-  for (int i = 0; i < 10; ++i) {
-    printf("%.1f, %.1f\n", h_x[i], h_y[i]);
+  // Test 1D RNG adapted from example
+  {
+    printf("1D RNG\n");
+    thrust::device_vector<float> d_x(numPts);
+    Random_generator1d rnd;
+
+    for (int i = 0; i < repeats; ++i) {
+      thrust::generate(d_x.begin(), d_x.end(), rnd);
+    }
+    thrust::host_vector<float> h_x = d_x;
+
+    for (int i = 0; i < 10; ++i) {
+      printf("%.3f\n", h_x[i]);
+    }
   }
+  printf("====================================\n");
+
+  // Test 1D RNG with sincos to make complex
+  {
+    printf("1D RNG with sincos\n");
+    Random_generator1d_phase rnd;
+
+    thrust::device_vector<thrust::complex<float>> d_z(numPts);
+
+    for (int i = 0; i < repeats; ++i) {
+      thrust::generate(d_z.begin(), d_z.end(), rnd);
+    }
+
+    thrust::host_vector<thrust::complex<float>> h_z = d_z;
+
+    for (int i = 0; i < 10; ++i) {
+      printf("%.3f, %.3f\n", h_z[i].real(), h_z[i].imag());
+    }
+  }
+  printf("====================================\n");
+
+  // Test 1D RNG with sincos to make complex, then multiply with something
+  {
+    printf("1D RNG with sincos, then mul\n");
+    Random_generator1d_phase_cplxMul rnd;
+
+    thrust::device_vector<thrust::complex<float>> d_z(numPts);
+
+    // Generate some numbers to multiply by
+    thrust::device_vector<thrust::complex<float>> d_w(numPts);
+    thrust::sequence(d_w.begin(), d_w.end());
+    thrust::host_vector<thrust::complex<float>> h_w = d_w;
+
+    for (int i = 0; i < repeats; ++i) {
+      thrust::transform(d_w.begin(), d_w.end(), d_z.begin(), rnd);
+    }
+
+    thrust::host_vector<thrust::complex<float>> h_z = d_z;
+
+    for (int i = 0; i < 10; ++i) {
+      printf("%.3f, %.3f <- %.3f, %.3f\n", h_z[i].real(), h_z[i].imag(),
+             h_w[i].real(), h_w[i].imag());
+    }
+  }
+  printf("====================================\n");
 
   return 0;
 }
---
+++ proj_rng/thrust_extensions.h
@@ -0,0 +1,130 @@
+/*
+ * Here we define RNG-related structs or functions that use thrust.
+ * These usually depend on vector-wide transform() or generate() functions
+ * to implement the operator() function of the struct that is passed.
+ */
+
+#include <thrust/random.h>
+#include <thrust/complex.h>
+
+// The following is adapted from
+// cuda-samples/Samples/3_CUDA_Features/cdpQuadtree/cdpQuadtree.cu
+struct Random_generator2d {
+
+  int count;
+  __host__ __device__ Random_generator2d() : count(0) {}
+  __host__ __device__ unsigned int hash(unsigned int a) {
+    a = (a + 0x7ed55d16) + (a << 12);
+    a = (a ^ 0xc761c23c) ^ (a >> 19);
+    a = (a + 0x165667b1) + (a << 5);
+    a = (a + 0xd3a2646c) ^ (a << 9);
+    a = (a + 0xfd7046c5) + (a << 3);
+    a = (a ^ 0xb55a4f09) ^ (a >> 16);
+    return a;
+  }
+
+  __host__ __device__ __forceinline__ thrust::tuple<float, float> operator()() {
+
+    // #ifdef __CUDA_ARCH__
+    unsigned seed = hash(blockIdx.x * blockDim.x + threadIdx.x + count);
+    // thrust::generate may call operator() more than once per thread.
+    // Hence, increment count by grid size to ensure uniqueness of seed
+    count += blockDim.x * gridDim.x;
+
+    // #else
+    //
+    //     unsigned seed = hash(0);
+    //
+    // #endif
+
+    thrust::random::default_random_engine rng(seed);
+    thrust::random::uniform_real_distribution<float> distrib;
+
+    return thrust::make_tuple(distrib(rng), distrib(rng));
+  }
+};
+
+// Make a 1d flavour
+struct Random_generator1d {
+  int count;
+  __host__ __device__ Random_generator1d() : count(0) {}
+  __host__ __device__ unsigned int hash(unsigned int a) {
+    a = (a + 0x7ed55d16) + (a << 12);
+    a = (a ^ 0xc761c23c) ^ (a >> 19);
+    a = (a + 0x165667b1) + (a << 5);
+    a = (a + 0xd3a2646c) ^ (a << 9);
+    a = (a + 0xfd7046c5) + (a << 3);
+    a = (a ^ 0xb55a4f09) ^ (a >> 16);
+    return a;
+  }
+
+  __host__ __device__ __forceinline__ float operator()() {
+    unsigned seed = hash(blockIdx.x * blockDim.x + threadIdx.x + count);
+    // thrust::generate may call operator() more than once per thread.
+    // Hence, increment count by grid size to ensure uniqueness of seed
+    count += blockDim.x * gridDim.x;
+    thrust::random::default_random_engine rng(seed);
+    thrust::random::uniform_real_distribution<float> distrib;
+
+    return distrib(rng);
+  }
+};
+
+// 1D flavour that generates random phase to complex values
+// i.e. exp(1i*rng)
+struct Random_generator1d_phase {
+  int count;
+  __host__ __device__ Random_generator1d_phase() : count(0) {}
+  __host__ __device__ unsigned int hash(unsigned int a) {
+    a = (a + 0x7ed55d16) + (a << 12);
+    a = (a ^ 0xc761c23c) ^ (a >> 19);
+    a = (a + 0x165667b1) + (a << 5);
+    a = (a + 0xd3a2646c) ^ (a << 9);
+    a = (a + 0xfd7046c5) + (a << 3);
+    a = (a ^ 0xb55a4f09) ^ (a >> 16);
+    return a;
+  }
+
+  __host__ __device__ __forceinline__ thrust::complex<float> operator()() {
+    unsigned seed = hash(blockIdx.x * blockDim.x + threadIdx.x + count);
+    // thrust::generate may call operator() more than once per thread.
+    // Hence, increment count by grid size to ensure uniqueness of seed
+    count += blockDim.x * gridDim.x;
+    thrust::random::default_random_engine rng(seed);
+    thrust::random::uniform_real_distribution<float> distrib;
+
+    float phase = distrib(rng);
+
+    return thrust::complex<float>(cosf(phase), sinf(phase));
+  }
+};
+
+// 1D phase to complex with a multiply
+struct Random_generator1d_phase_cplxMul {
+  int count;
+  __host__ __device__ Random_generator1d_phase_cplxMul() : count(0) {}
+  __host__ __device__ unsigned int hash(unsigned int a) {
+    a = (a + 0x7ed55d16) + (a << 12);
+    a = (a ^ 0xc761c23c) ^ (a >> 19);
+    a = (a + 0x165667b1) + (a << 5);
+    a = (a + 0xd3a2646c) ^ (a << 9);
+    a = (a + 0xfd7046c5) + (a << 3);
+    a = (a ^ 0xb55a4f09) ^ (a >> 16);
+    return a;
+  }
+
+  __host__ __device__ __forceinline__ thrust::complex<float> operator()(
+    const thrust::complex<float>& input
+  ) {
+    unsigned seed = hash(blockIdx.x * blockDim.x + threadIdx.x + count);
+    // thrust::generate may call operator() more than once per thread.
+    // Hence, increment count by grid size to ensure uniqueness of seed
+    count += blockDim.x * gridDim.x;
+    thrust::random::default_random_engine rng(seed);
+    thrust::random::uniform_real_distribution<float> distrib;
+
+    float phase = distrib(rng);
+
+    return thrust::complex<float>(cosf(phase), sinf(phase)) * input;
+  }
+};
