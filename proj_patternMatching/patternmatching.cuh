74fa506ffbbccbd6903a10122edabbad15515752
icyveins7 <icyveins@hotmail.com>
2025-03-13 23:20:50 +0800

    fix namespace and add docs to patternmatching.cuh

--- proj_patternMatching/patternmatching.cuh
+++ proj_patternMatching/patternmatching.cuh
@@ -5,31 +5,44 @@ namespace cg = cooperative_groups;
 
 // Directly copied from https://developer.nvidia.com/blog/cooperative-groups/
 template <typename T>
-__device__ T reduce_sum(thread_group g, T *temp, T val)
-{
-    int lane = g.thread_rank();
+__device__ T reduce_sum(cg::thread_group g, T *temp, T val) {
+  int lane = g.thread_rank();
 
-    // Each iteration halves the number of active threads
-    // Each thread adds its partial sum[i] to sum[lane+i]
-    for (int i = g.size() / 2; i > 0; i /= 2)
-    {
-        temp[lane] = val;
-        g.sync(); // wait for all threads to store
-        if(lane<i) val += temp[lane + i];
-        g.sync(); // wait for all threads to load
-    }
-    return val; // note: only thread 0 will return full sum
+  // Each iteration halves the number of active threads
+  // Each thread adds its partial sum[i] to sum[lane+i]
+  for (int i = g.size() / 2; i > 0; i /= 2) {
+    temp[lane] = val;
+    g.sync(); // wait for all threads to store
+    if (lane < i)
+      val += temp[lane + i];
+    g.sync(); // wait for all threads to load
+  }
+  return val; // note: only thread 0 will return full sum
 }
 
+/**
+ * @brief Naive block-to-output implementation of pattern matching kernel,
+ * computing least squares residuals. Each block works on a single pattern and a
+ * single input vector.
+ *
+ * @tparam T Type of input and pattern
+ * @tparma U Type of metric calculations
+ * @param d_inputs Array of input vectors, stacked contiguously
+ * @param numInputs Number of array input vectors
+ * @param d_patterns Array of pattern vectors, stacked contiguously
+ * @param numPatterns Number of array pattern vectors
+ * @param length Length of each input/pattern vector
+ * @param d_metric Output array
+ */
 template <typename T, typename U = T>
-__global__ void naivePatternMatchKernel_LS(
-  const T* d_inputs, // numInputs * length
-  const unsigned int numInputs,
-  const T* d_patterns, // numPatterns * length
-  const unsigned int numPatterns,
-  const unsigned int length,
-  U* d_metric // numInputs * numPatterns
-){
+__global__ void
+naivePatternMatchKernel_LS(const T *d_inputs, // numInputs * length
+                           const unsigned int numInputs,
+                           const T *d_patterns, // numPatterns * length
+                           const unsigned int numPatterns,
+                           const unsigned int length,
+                           U *d_metric // numInputs * numPatterns
+) {
   // Retrieve the input and pattern that this block is working on
   const unsigned int pIdx = blockIdx.x;
   const unsigned int iIdx = blockIdx.y;
@@ -40,28 +53,26 @@ __global__ void naivePatternMatchKernel_LS(
 
   // Retrieve shared memory
   SharedMemory<U> smem;
-  U* s_workspace = smem.getPointer(); // assume 1 for each thread
+  U *s_workspace = smem.getPointer(); // assume 1 for each thread
 
   // Otherwise get a pointer to the start of both vectors
-  const T* b_input = d_input[iIdx * length];
-  const T* b_pattern = d_patterns[pIdx * length];
+  const T *b_input = d_inputs[iIdx * length];
+  const T *b_pattern = d_patterns[pIdx * length];
 
   // Perform the least squares residual calculation on each vector
   U diff;
   U metric = 0;
-  for (int t = threadIdx.x; t < length; t += blockDim.x)
-  {
+  for (int t = threadIdx.x; t < length; t += blockDim.x) {
     diff = b_input[t] - b_pattern[t];
     // Accumulated into local thread workspace first
-    metric += diff*diff;
+    metric += diff * diff;
   }
 
   // Block reduce to a sum
   __syncthreads();
-  auto g = this_thread_block();
+  auto g = cg::this_thread_block();
   U sum = reduce_sum(g, s_workspace, metric);
 
   if (g.thread_rank() == 0)
     d_metric = sum; // no atomic needed here since 1 block writes 1 val
-
 }
