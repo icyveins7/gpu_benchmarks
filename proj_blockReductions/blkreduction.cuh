4dbe51dba0059127e397ac5785ae2a2a7bd04afc
icyveins7 <icyveins@hotmail.com>
2025-04-25 12:23:36 +0800

    complete initial implementation of maxArgMax block reduction & example
    can probably port it to an actual test later

--- proj_blockReductions/blkreduction.cuh
+++ proj_blockReductions/blkreduction.cuh
@@ -7,26 +7,76 @@ static_assert(true); // dummy assert just for clangd LSP to stop complaining
 
 /**
  * @brief Max and argmax reduction within a block. This does not currently
- * (24/4/2025) seem to have a simple cub implementation.
+ * (24/4/2025) seem to have a simple cub implementation. The max and argmax are
+ * in the index 0 element of each array at the end, as usual.
  *
  * @tparam T Type of input
  * @param sdata Input data in shared memory, assumed to have length blockDim.x
- * @param idx Associated indices of the input data
+ * @param idx Associated indices of the input data in shared memory, same length
  */
 template <typename T>
-__device__ void blockReduceMaxAndArgMax(T *sdata, int *idx) {
+__device__ void blockReduceMaxAndArgMax(T *sdata, int *sidx) {
   // We won't assume the user will sync before this.
   __syncthreads();
 
   // Reduce inside shared memory
   for (int s = blockDim.x / 2; s > 0; s >>= 1) {
     if (threadIdx.x < s) {
-      if (sdata[threadIdx.x] < sdata[threadIdx.x + s]) {
+      if (sdata[threadIdx.x + s] >= sdata[threadIdx.x]) {
+        // Prefer the later index if equal
+        sidx[threadIdx.x] = sdata[threadIdx.x] == sdata[threadIdx.x + s]
+                                ? max(sidx[threadIdx.x], sidx[threadIdx.x + s])
+                                : sidx[threadIdx.x + s];
         sdata[threadIdx.x] = sdata[threadIdx.x + s];
-        idx[threadIdx.x] = idx[threadIdx.x + s];
       }
     }
     __syncthreads();
+    // if (blockIdx.x == 25 && threadIdx.x < s)
+    //   printf("Batch %d thread %d, [%d]: %d\n", s, threadIdx.x,
+    //          sidx[threadIdx.x], sdata[threadIdx.x]);
+  }
+}
+
+template <typename T>
+__global__ void simpleBlockMaxAndArgMaxKernel(
+    const T *in, const int inLenPerBlock, T *maxPerBlock, int *argMaxPerBlock,
+    T *debugMaxPerBlock = nullptr, int *debugArgMaxPerBlock = nullptr) {
+  // Get the input for the block
+  const T *blk_in = &in[blockIdx.x * inLenPerBlock];
+
+  // Get some shared mem for 1 block
+  SharedMemory<T> smem;
+  T *s_in = smem.getPointer();
+  int *s_idx = (int *)&s_in[blockDim.x];
+  // Initialize some starting values for each thread in the block
+  s_in[threadIdx.x] = 0;
+  s_idx[threadIdx.x] = -1;
+
+  // Compare while loading
+  for (int t = threadIdx.x; t < inLenPerBlock; t += blockDim.x) {
+    T candidate = blk_in[t];
+    if (candidate >= s_in[threadIdx.x]) {
+      // Prefer the later index if equal
+      s_idx[threadIdx.x] =
+          candidate == s_in[threadIdx.x] ? max(s_idx[threadIdx.x], t) : t;
+      s_in[threadIdx.x] = blk_in[t];
+    }
+  }
+
+  // Debug shared memory states
+  if (debugMaxPerBlock != nullptr) {
+    debugMaxPerBlock[blockIdx.x * blockDim.x + threadIdx.x] = s_in[threadIdx.x];
+  }
+  if (debugArgMaxPerBlock != nullptr) {
+    debugArgMaxPerBlock[blockIdx.x * blockDim.x + threadIdx.x] =
+        s_idx[threadIdx.x];
+  }
+
+  blockReduceMaxAndArgMax(s_in, s_idx);
+
+  if (threadIdx.x == 0) {
+    maxPerBlock[blockIdx.x] = s_in[0];
+    argMaxPerBlock[blockIdx.x] = s_idx[0];
   }
 }
 
--- proj_blockReductions/blkreductions.cu
+++ proj_blockReductions/blkreductions.cu
@@ -1,7 +1,84 @@
+#include "blkreduction.cuh"
+#include <cstdlib>
 #include <iostream>
+#include <thrust/device_vector.h>
+#include <thrust/host_vector.h>
 
 int main() {
   printf("Block-wise reductions.\n");
+
+  int batch = 26; // 32;
+  int length = 1024;
+  thrust::host_vector<int> src(batch * length);
+  thrust::host_vector<int> maxPerBlock(batch);
+  thrust::host_vector<int> argMaxPerBlock(batch);
+
+  // Fill with some random data
+  for (int i = 0; i < batch * length; i++) {
+    src[i] = rand() % 100;
+  }
+
+  thrust::device_vector<int> d_src = src;
+  thrust::device_vector<int> d_maxPerBlock(maxPerBlock.size());
+  thrust::device_vector<int> d_argMaxPerBlock(argMaxPerBlock.size());
+
+  // Run kernel
+  int blocks = batch;
+  int threads_per_blk = 128;
+  int smem = threads_per_blk * (sizeof(int) + sizeof(int));
+
+  thrust::device_vector<int> d_debugMaxPerBlockStates(threads_per_blk * blocks);
+  thrust::device_vector<int> d_debugArgMaxPerBlockStates(threads_per_blk *
+                                                         blocks);
+
+  simpleBlockMaxAndArgMaxKernel<<<blocks, threads_per_blk, smem>>>(
+      d_src.data().get(), length, d_maxPerBlock.data().get(),
+      d_argMaxPerBlock.data().get(), d_debugMaxPerBlockStates.data().get(),
+      d_debugArgMaxPerBlockStates.data().get());
+
+  maxPerBlock = d_maxPerBlock;
+  argMaxPerBlock = d_argMaxPerBlock;
+
+  thrust::host_vector<int> debugMaxPerBlockStates = d_debugMaxPerBlockStates;
+  thrust::host_vector<int> debugArgMaxPerBlockStates =
+      d_debugArgMaxPerBlockStates;
+
+  // Check
+  for (int i = 0; i < batch; ++i) {
+    printf("Batch %d\n", i);
+    printf("Max: %d -> %d (%d directly)\n", argMaxPerBlock[i],
+           src[i * length + argMaxPerBlock[i]], maxPerBlock[i]);
+
+    // // Shared memory inspection
+    // int maxInShmem = 0;
+    // int argMaxInShmem = -1;
+    // for (int t = 0; t < threads_per_blk; ++t) {
+    //   printf("SHM [%4d]: %4d -> %4d\n", t,
+    //          debugArgMaxPerBlockStates[i * threads_per_blk + t],
+    //          debugMaxPerBlockStates[i * threads_per_blk + t]);
+    //   if (debugMaxPerBlockStates[i * threads_per_blk + t] >= maxInShmem) {
+    //     maxInShmem = debugMaxPerBlockStates[i * threads_per_blk + t];
+    //     argMaxInShmem = std::max(
+    //         debugArgMaxPerBlockStates[i * threads_per_blk + t],
+    //         argMaxInShmem);
+    //   }
+    // }
+    // printf("Shared memory max: %d -> %d\n", argMaxInShmem, maxInShmem);
+    // printf("Verify shared memory is valid: %d -> %d\n", argMaxInShmem,
+    //        src[i * length + argMaxInShmem]);
+
+    // Direct check on host data
+    int checkMax = 0, checkArgmax = -1;
+    for (int j = 0; j < length; ++j) {
+      if (src[i * length + j] >= checkMax) {
+        checkMax = src[i * length + j];
+        checkArgmax = j;
+      }
+    }
+    printf("Check %d -> %d (%d directly)\n", checkArgmax,
+           src[i * length + checkArgmax], checkMax);
+    printf("=========\n");
+  }
 
   return 0;
 }
