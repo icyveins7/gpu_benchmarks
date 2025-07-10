2e347c7c2ed3d869970b1ec25d15e340b5aab893
icyveins7 <icyveins@hotmail.com>
2025-07-10 16:36:39 +0800

    update the new minmax-hold kernel with detailed docstring

--- proj_outlierHandling/outlierOverwriting.cuh
+++ proj_outlierHandling/outlierOverwriting.cuh
@@ -15,11 +15,43 @@
  *   4) Overwrite outliers (beyond thresholds) in original data
  */
 
+/**
+ * @brief Performs a min/max-hold by overwriting elements in the data that
+ * extend beyond median +/- minmaxStdFactor*std, for each section.
+ *
+ * Both the median value and the std are assumed to be estimated elsewhere, and
+ * these statistics are used as representative values when overwriting values in
+ * the larger, original data. See medians.cuh for more details.
+ *
+ * Original use case:
+ *   1) Original data is subsampled, per section
+ *   2) Subsampled sections have outliers removed iteratively (invalid elements
+ * overwritten, see outlierRemoval.cuh)
+ *   3) Remaining subsampled sections have medians estimated (see medians.cuh)
+ *   4) This kernel is called, using the previous kernels' output to determine
+ * thresholds, and overwrite those that exceed it
+ *
+ * @tparam Tdata Data type for input/medians
+ * @tparam Tsum Data type for the sum and sum of squares
+ * @tparam Tcalc Data type used for internal calculations
+ * @tparam Tsize Data type for sizes like counters
+ * @param d_data Larger, original input data (still has sections)
+ * @param dataLengthPerSection Length of each section in d_data
+ * @param numSections Number of sections
+ * @param sectionMedians Medians of each section to use for thresholds
+ * @param d_sumSections Sum per section, used in mean/std calculations
+ * @param d_sumSqSections Sum of squares per section, similar to sum
+ * @param validSectionSizes Counter of valid elements used in the sum/sumSq
+ * @param minmaxStdFactor Factor to multiply standard deviation by, for
+ * thresholds
+ */
 template <typename Tdata, typename Tsum, typename Tcalc, typename Tsize>
 __global__ void minmax_hold_from_section_medians_thresholds_kernel(
     Tdata *d_data, Tsize dataLengthPerSection, Tsize numSections,
     const Tdata *sectionMedians, Tsum *d_sumSections, Tsum *d_sumSqSections,
     Tsize *validSectionSizes, const Tcalc minmaxStdFactor) {
+  static_assert(cuda::std::is_floating_point_v<Tcalc>,
+                "Tcalc must be float or double");
   // Same as before, assume we operate on each section via blockIdx.y
   Tsize sectionIdx = blockIdx.y;
   if (sectionIdx >= numSections) {
