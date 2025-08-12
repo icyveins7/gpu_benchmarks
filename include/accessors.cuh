61f01a375ea48d0940678aac03eacf6e1d02ebe5
icyveins7 <icyveins@hotmail.com>
2025-08-12 16:14:02 +0800

    include blockwise roi load instead of gridwise, for shared mem

--- include/accessors.cuh
+++ include/accessors.cuh
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <stdio.h>
+
 /**
  * @brief Converts ROI coordinates to the original source image coordinates.
  *
@@ -55,16 +57,73 @@ __device__ void fromRoiCoords(const int roiX, const int roiY,
  * @param dst Destination, can be global or shared
  */
 template <typename T>
-__device__ void blockRoiLoad(const T *src, const int srcWidth,
-                             const int srcHeight, const int roiStartX,
-                             const int roiLengthX, const int roiStartY,
-                             const int roiLengthY, T *dst) {
+__device__ void roiLoad(const T *src, const int srcWidth, const int srcHeight,
+                        const int roiStartX, const int roiLengthX,
+                        const int roiStartY, const int roiLengthY, T *dst) {
   // Grid-stride over the destination; tx and ty are the roi-zeroed coords
   int x, y;
   for (int ty = threadIdx.y + blockDim.y * blockIdx.y; ty < roiLengthY;
        ty += blockDim.y * gridDim.y) {
     for (int tx = threadIdx.x + blockDim.x * blockIdx.x; tx < roiLengthX;
          tx += blockDim.x * gridDim.x) {
+      fromRoiCoords(tx, ty, roiStartX, roiStartY, x, y);
+      if (x >= 0 && x < srcWidth && y >= 0 && y < srcHeight)
+        dst[ty * roiLengthX + tx] = src[y * srcWidth + x];
+    }
+  }
+}
+
+/**
+ * @brief Loads an ROI from a source image to a packed destination, whose
+ * dimensions equal the ROI. Usually used for shared memory, or any other
+ * operation involving only one block.
+ *
+ * @detail This block-specific loader is useful in contexts where each CUDA
+ * block works on its own ROI/tile. This loader is both source and destination
+ * dimensions-aware, meaning it will not read from the source or write to the
+ * destination if either source or destination indices exceed their respective
+ * heights/widths.
+ *
+ * NOTE: this expects that all threads in the block are active at the point of
+ * invocation. The programmer must remember not to do an early 'return' at the
+ * start of the kernel for some threads, even if they are not used for later
+ * results, as this will affect the block-wise iteration over the ROI i.e. some
+ * values will be missed.
+ *
+ * @example
+ * Below is an example of the ROI load, where the image is presented top-down
+ * i.e. row 0 is the top-most row.
+ *    srcWidth = 9
+ *    srcHeight = 4
+ *    roiStartX = 3
+ *    roiStartY = 1
+ *    roiLengthX = 3
+ *    roiLengthY = 2
+ *
+ * XXXXXXXXX
+ * XXXABCXXX    ->   ABC
+ * XXXDEFXXX    ->   DEF
+ * XXXXXXXXX
+ *
+ * @tparam T Type of data
+ * @param src Source image
+ * @param srcWidth Source image width
+ * @param srcHeight Source image height
+ * @param roiStartX Starting X pixel of ROI
+ * @param roiLengthX Length of ROI in X
+ * @param roiStartY Starting Y pixel of ROI
+ * @param roiLengthY Length of ROI in Y
+ * @param dst Destination, can be global or shared
+ */
+template <typename T>
+__device__ void blockRoiLoad(const T *src, const int srcWidth,
+                             const int srcHeight, const int roiStartX,
+                             const int roiLengthX, const int roiStartY,
+                             const int roiLengthY, T *dst) {
+  // Block-stride over the destination
+  int x, y;
+  for (int ty = threadIdx.y; ty < roiLengthY; ty += blockDim.y) {
+    for (int tx = threadIdx.x; tx < roiLengthX; tx += blockDim.x) {
       fromRoiCoords(tx, ty, roiStartX, roiStartY, x, y);
       if (x >= 0 && x < srcWidth && y >= 0 && y < srcHeight)
         dst[ty * roiLengthX + tx] = src[y * srcWidth + x];
