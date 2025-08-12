3aa7153149b5c93dc38de779113592dc0319f8d6
icyveins7 <icyveins@hotmail.com>
2025-08-12 10:05:09 +0800

    add simple copy test for accessors

---
+++ proj_selections/test_accessors.cu
@@ -0,0 +1,45 @@
+#include "accessors.cuh"
+
+#include <cstdlib>
+#include <thrust/device_vector.h>
+#include <thrust/host_vector.h>
+
+#include <gtest/gtest.h>
+#include <thrust/sequence.h>
+
+template <typename T>
+__global__ void
+test_blockRoiLoad_kernel(const T *src, T *dst, const int srcWidth,
+                         const int srcHeight, const int roiStartX,
+                         const int roiStartY, const int roiLengthX,
+                         const int roiLengthY) {
+  // Load and copy directly
+  blockRoiLoad<T>(src, srcWidth, srcHeight, roiStartX, roiLengthX, roiStartY,
+                  roiLengthY, dst);
+}
+
+template <typename T>
+void test_blockRoiLoad(const int srcWidth, const int srcHeight,
+                       const int roiStartX, const int roiStartY,
+                       const int roiLengthX, const int roiLengthY) {
+  thrust::device_vector<T> d_src(srcWidth * srcHeight);
+  thrust::sequence(d_src.begin(), d_src.end());
+  thrust::device_vector<T> d_dst(roiLengthX * roiLengthY);
+
+  test_blockRoiLoad_kernel<<<4, 32>>>(d_src.data().get(), d_dst.data().get(),
+                                      srcWidth, srcHeight, roiStartX, roiStartY,
+                                      roiLengthX, roiLengthY);
+
+  thrust::host_vector<T> h_dst = d_dst;
+
+  for (int i = 0; i < roiLengthY; ++i) {
+    for (int j = 0; j < roiLengthX; ++j) {
+      EXPECT_EQ(h_dst[i * roiLengthX + j],
+                (i + roiStartY) * srcWidth + j + roiStartX);
+    }
+  }
+}
+
+TEST(Accessors, blockRoiLoad) {
+  test_blockRoiLoad<unsigned short>(9, 4, 3, 1, 3, 2);
+}
