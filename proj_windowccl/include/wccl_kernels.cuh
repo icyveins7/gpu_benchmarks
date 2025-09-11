db76afb084d483dfdf2af0a9e969bb44202dedc2
icyveins7 <icyveins@hotmail.com>
2025-09-11 16:12:29 +0800

    encapsulate most unionfind logic required into subclass DeviceIndexImage
    this struct which inherits from DeviceImage handles the pairwise types
    like short2 and has most of the logic refactored into it, like
    making/checking invalid row/col indices for inactive sites, accounting
    for global offset of the tile etc. the logic for unite() and find() now
    explicitly use this struct type, so it is slightly clearer imo. the
    entire shared mem logic is now calculated using this struct type which
    uses the pair row/col indices, as this is required to more easily
    account for the global row/col offset when writing back to global mem
    (instead of flattening to 1D and then having to perform divisions to go
    back to row/col so we can offset it)

--- proj_windowccl/include/wccl_kernels.cuh
+++ proj_windowccl/include/wccl_kernels.cuh
@@ -18,14 +18,10 @@ template <typename T> struct DeviceImage {
   T *data; // non-owning, but we leave as raw pointer for CUDA stuff
   unsigned int height;
   unsigned int width;
-  unsigned int yOffset = 0; // only used when we are looking
-  unsigned int xOffset = 0; // at a tile inside kernels
 
   DeviceImage() = default;
-  __device__ DeviceImage(T *_data, int _height, int _width, int _yOffset = 0,
-                         int _xOffset = 0)
-      : data(_data), height(_height), width(_width), yOffset(_yOffset),
-        xOffset(_xOffset) {}
+  __device__ DeviceImage(T *_data, int _height, int _width)
+      : data(_data), height(_height), width(_width) {}
   DeviceImage(thrust::device_vector<T> &_data, int _height, int _width)
       : data(_data.data().get()), height(_height), width(_width) {
     if (data == nullptr) {
@@ -47,14 +43,6 @@ template <typename T> struct DeviceImage {
                                                   unsigned int col) const {
     return row * width + col;
   }
-  /**
-   * @brief Returns the global flattened 1D index from the row and column
-   * indices. This accounts for the offsets of the current tile.
-   */
-  __host__ __device__ unsigned int globalIndex(unsigned int row,
-                                               unsigned int col) const {
-    return (row + yOffset) * width + (col + xOffset);
-  }
 
   /**
    * @brief Range-checked getter via row/col indices.
@@ -62,7 +50,7 @@ template <typename T> struct DeviceImage {
   __host__ __device__ T get(const unsigned int row,
                             const unsigned int col) const {
     if (row >= height || col >= width)
-      return cuda::std::numeric_limits<T>::max();
+      return T(); // use default-constructed value?
     return data[flattenedIndex(row, col)];
   }
 
@@ -76,57 +64,158 @@ template <typename T> struct DeviceImage {
   }
 };
 
-template <typename T>
-__device__ T find(const DeviceImage<T> &tile, const unsigned int row,
-                  const unsigned int col) {
-  unsigned int idx = tile.flattenedIndex(row, col);
+/**
+ * @brief Specialization that should hold a pair type, representing row and
+ * column indices.
+ *
+ * @tparam T Pair type, char2 or short2 or int2. Convention is {x, y} i.e. {col,
+ * row}.
+ */
+template <typename Tcolrow>
+struct DeviceIndexImage : public DeviceImage<Tcolrow> {
+  static_assert(std::is_same_v<Tcolrow, char2> ||
+                    std::is_same_v<Tcolrow, short2> ||
+                    std::is_same_v<Tcolrow, int2>,
+                "T must be either char2, short2 or int2");
+
+  __device__ DeviceIndexImage() = default;
+  __device__ DeviceIndexImage(Tcolrow *_data, int _height, int _width)
+      : DeviceImage<Tcolrow>(_data, _height, _width) {}
+
+  __device__ bool isInvalidAt(const unsigned int row,
+                              const unsigned int col) const {
+    Tcolrow element = this->data[this->flattenedIndex(row, col)];
+    return element.x < 0 || element.y < 0;
+  }
+
+  __device__ void makeInvalidAt(const unsigned int row,
+                                const unsigned int col) {
+    Tcolrow element;
+    element.x = -1;
+    element.y = -1;
+    this->data[this->flattenedIndex(row, col)] = element;
+  }
+
+  /**
+   * @brief Returns True if the element at row, col contains (col, row).
+   */
+  __device__ bool isSelfRoot(const unsigned int row,
+                             const unsigned int col) const {
+    Tcolrow element = this->data[this->flattenedIndex(row, col)];
+    return element.y == row && element.x == col;
+  }
+
+  /**
+   * @brief Fills the element at row, col with (col, row).
+   */
+  __device__ void setInnateValueAt(const unsigned int row,
+                                   const unsigned int col) {
+    Tcolrow element{col, row};
+    this->data[this->flattenedIndex(row, col)] = element;
+  }
+
+  /**
+   * @brief Converts the internal row/col indices into a globally offset
+   * flattened index.
+   * @detail Primarily used when performing a final storage into global memory,
+   * to account for tile offsets based on the current block. Specifically
+   * ignores negative values (returns -1).
+   *
+   * @tparam U Type of global memory index
+   * @param row The internal (local) row index to look up
+   * @param col The internal (local) col index to look up
+   * @param rowOffset Row offset of this image with respect to the global image
+   * @param colOffset Col offset of this image with respect to the global image
+   * @param globalWidth Global image width
+   * @return
+   */
+  template <typename U>
+  __device__ U readAndConvertToGlobalIndexFrom(const unsigned int row,
+                                               const unsigned int col,
+                                               const unsigned int rowOffset,
+                                               const unsigned int colOffset,
+                                               const unsigned int globalWidth) {
+    if (this->isInvalidAt(row, col))
+      return U(-1);
+    // Calculate global row/col
+    Tcolrow element = this->data[this->flattenedIndex(row, col)];
+    Tcolrow globalColRow{element.x + colOffset, element.y + rowOffset};
+    // Flatten the global row/col into 1D index
+    return (U)(globalColRow.y * globalWidth + globalColRow.x);
+  }
+};
+
+// ========================================================================
+// ========================================================================
+// ======================== KERNELS AND DEVICE FUNCS ======================
+// ========================================================================
+// ========================================================================
+
+template <typename Tcolrow>
+__device__ Tcolrow find(const DeviceIndexImage<Tcolrow> &tile, Tcolrow idx) {
   // Return directly if inactive i.e. -1 -> -1
-  if (tile.data[idx] < 0)
-    return tile.data[idx];
+  if (tile.isInvalidAt(idx.y, idx.x))
+    return tile.get(idx.y, idx.x);
 
   // Walk entire thing to find root
-  while (tile.data[idx] != idx) {
-    idx = tile.data[idx];
+  while (!tile.isSelfRoot(idx.y, idx.x)) {
+    idx = tile.get(idx.y, idx.x);
   }
   return idx;
 }
 
-template <typename T>
-__device__ void unite(const DeviceImage<T> &tile_in, DeviceImage<T> &tile_out,
+template <typename Tcolrow, typename Tmapping>
+__device__ void unite(const DeviceIndexImage<Tcolrow> &tile_in,
+                      DeviceIndexImage<Tcolrow> &tile_out,
                       const int2 windowDist) {
   for (int ty = threadIdx.y; ty < tile_in.height; ty += blockDim.y) {
     for (int tx = threadIdx.x; tx < tile_in.width; tx += blockDim.x) {
-      T root = find(tile_in, ty, tx);
-
-      if (root >= 0) {
-        // Loop over the surrounding window
-        for (int wy = ty - windowDist.y; wy <= ty + windowDist.y; wy++) {
-          for (int wx = tx - windowDist.x; wx <= tx + windowDist.x; wx++) {
-            if (wy >= 0 && wy < tile_in.height && wx >= 0 &&
-                wx < tile_in.width) {
-              T windowRoot = find(tile_in, wy, wx);
-              if (windowRoot < 0)
-                continue;
-
-              // Take min root
-              root = root < windowRoot ? root : windowRoot;
-            }
-          }
-        }
+      // Early exit (but still remember to copy the invalid value)
+      if (tile_in.isInvalidAt(ty, tx)) {
+        tile_out.makeInvalidAt(ty, tx);
+        continue;
       }
 
+      Tcolrow idx = Tcolrow{tx, ty};
+      Tcolrow root = find(tile_in, idx);
+      // Determine 1D index for comparisons
+      Tmapping flatroot = tile_in.flattenedIndex(root.y, root.x);
+
+      // Loop over the surrounding window
+      for (int wy = ty - windowDist.y; wy <= ty + windowDist.y; wy++) {
+        for (int wx = tx - windowDist.x; wx <= tx + windowDist.x; wx++) {
+          if (wy >= 0 && wy < tile_in.height && wx >= 0 && wx < tile_in.width &&
+              !tile_in.isInvalidAt(wy, wx) // If inactive, ignore
+          ) {
+            Tcolrow widx = Tcolrow{wx, wy};
+            Tcolrow windowRoot = find(tile_in, widx);
+
+            // Determine 1D index for comparisons
+            Tmapping flatwindowRoot =
+                tile_in.flattenedIndex(windowRoot.y, windowRoot.x);
+
+            // Take min root
+            root = flatroot < flatwindowRoot ? root : windowRoot;
+            // remember to recompute the flatroot for comparisons!
+            flatroot = tile_in.flattenedIndex(root.y, root.x);
+            // printf("flatroot %d vs flatwindowRoot %d -> new root (%d, %d)\n",
+            //        flatroot, flatwindowRoot, root.y, root.x);
+          }
+        }
+      } // end loop over window
+
       // Write to output tile
-      printf("Wrote root %d to (%d, %d)\n", root, ty, tx);
+      // printf("Wrote root (%d,%d) to (%d, %d)\n", root.y, root.x, ty, tx);
       tile_out.set(ty, tx, root);
     }
   }
 }
 
-template <typename T>
+template <typename Tmapping, typename Tcolrow>
 __global__ void connect_kernel(const DeviceImage<uint8_t> input,
-                               DeviceImage<T> mapping, const int2 tileDims,
-                               const int2 windowDist) {
-  static_assert(std::is_signed_v<T>, "mapping type T must be signed");
+                               DeviceImage<Tmapping> mapping,
+                               const int2 tileDims, const int2 windowDist) {
+  static_assert(std::is_signed_v<Tmapping>, "mapping type T must be signed");
 
   // Define the tile for this block
   const int tileXstart = blockIdx.x * tileDims.x;
@@ -140,19 +229,18 @@ __global__ void connect_kernel(const DeviceImage<uint8_t> input,
                                : (int)input.height - tileYstart};
 
   // Read entire tile and set shared memory values
-  SharedMemory<T> smem;
+  SharedMemory<Tcolrow> smem;
   // Encapsulate shared memory tile for ergonomics
   // We have 2 workspaces
-  DeviceImage<T> s_tiles[2] = {
-      DeviceImage<T>(smem.getPointer(), blockTileDims.y, blockTileDims.x,
-                     tileYstart, tileXstart),
-      DeviceImage<T>(&s_tiles[0].data[s_tiles[0].size()], blockTileDims.y,
-                     blockTileDims.x, tileYstart, tileXstart)};
-
-  for (int ty = threadIdx.y; ty < tileDims.y; ty += blockDim.y) {
+  DeviceIndexImage<Tcolrow> s_tiles[2] = {
+      DeviceIndexImage<Tcolrow>(smem.getPointer(), blockTileDims.y,
+                                blockTileDims.x),
+      DeviceIndexImage<Tcolrow>(&s_tiles[0].data[s_tiles[0].size()],
+                                blockTileDims.y, blockTileDims.x)};
+  for (int ty = threadIdx.y; ty < blockTileDims.y; ty += blockDim.y) {
     // Define global index for reads (row)
     int y = tileYstart + ty;
-    for (int tx = threadIdx.x; tx < tileDims.x; tx += blockDim.x) {
+    for (int tx = threadIdx.x; tx < blockTileDims.x; tx += blockDim.x) {
       // Define global index for reads (col)
       int x = tileXstart + tx;
 
@@ -160,39 +248,74 @@ __global__ void connect_kernel(const DeviceImage<uint8_t> input,
       // set to flattened local index (for now)
       if (y < (int)input.height && x < (int)input.width) {
         const uint8_t site = input.get(y, x);
-        const T initVal = site > 0 ? s_tiles[0].flattenedIndex(ty, tx) : -1;
+        if (site > 0)
+          s_tiles[0].setInnateValueAt(ty, tx);
+        else
+          s_tiles[0].makeInvalidAt(ty, tx);
 
-        printf("tile(%d,%d)<-input.get(%d,%d): %d\n", ty, tx, y, x, initVal);
-        s_tiles[0].set(ty, tx, initVal);
+        printf("blk(%d,%d) tile(%d,%d)<-input.get(%d,%d): (%d,%d)\n",
+               blockIdx.x, blockIdx.y, ty, tx, y, x, s_tiles[0].get(ty, tx).y,
+               s_tiles[0].get(ty, tx).x);
       } else {
         // printf("Out of bounds at (%d, %d) in s_tiles[0]\n", y, x);
-        s_tiles[0].set(ty, tx, -1);
+        s_tiles[0].makeInvalidAt(ty, tx);
       }
     }
   }
   __syncthreads();
 
   // Perform unites
-  const int numUnites = 1;
-  for (int i = 0; i < numUnites; ++i) {
-    unite(s_tiles[i % 2], s_tiles[(i + 1) % 2], windowDist);
-    __syncthreads();
+  int numUniteSteps = 0;
+  constexpr int maxNumUnites = 100; // TODO: what's a good max iteration?
+  for (; numUniteSteps < maxNumUnites; ++numUniteSteps) {
+    unite<Tcolrow, Tmapping>(s_tiles[numUniteSteps % 2],
+                             s_tiles[(numUniteSteps + 1) % 2], windowDist);
+    // Detect workspace changes from input tile to output tile
+    int threadChanges = 0;
+    for (int ty = threadIdx.y; ty < blockTileDims.y; ty += blockDim.y) {
+      for (int tx = threadIdx.x; tx < blockTileDims.x; tx += blockDim.x) {
+        Tcolrow oldTileVal = s_tiles[numUniteSteps % 2].get(ty, tx);
+        Tcolrow newTileVal = s_tiles[(numUniteSteps + 1) % 2].get(ty, tx);
+        if (oldTileVal.x != newTileVal.x || oldTileVal.y != newTileVal.y) {
+          threadChanges++;
+        }
+      }
+    }
+    // Sync block and check if any changes were effected
+    int decision = __syncthreads_or(threadChanges);
+    if (decision == 0)
+      break;
   }
+  // if (threadIdx.x == 0 && threadIdx.y == 0) {
+  //   printf("numUniteSteps: %d\n", numUniteSteps);
+  //   for (int i = 0; i < blockTileDims.y; ++i) {
+  //     for (int j = 0; j < blockTileDims.x; ++j) {
+  //       printf("(%d, %d): (%d, %d)\n", i, j,
+  //              s_tiles[numUniteSteps % 2].get(i, j).y,
+  //              s_tiles[numUniteSteps % 2].get(i, j).x);
+  //     }
+  //   }
+  // }
 
-  // TODO: for now, just write this out to global
-  for (int ty = threadIdx.y; ty < tileDims.y; ty += blockDim.y) {
+  for (int ty = threadIdx.y; ty < blockTileDims.y; ty += blockDim.y) {
     int y = tileYstart + ty;
     if (y >= (int)mapping.height)
       continue;
-    for (int tx = threadIdx.x; tx < tileDims.x; tx += blockDim.x) {
+    for (int tx = threadIdx.x; tx < blockTileDims.x; tx += blockDim.x) {
       int x = tileXstart + tx;
       if (x >= (int)mapping.width)
         continue;
 
+      Tmapping globalIdx =
+          s_tiles[(numUniteSteps + 1) % 2]
+              .template readAndConvertToGlobalIndexFrom<Tmapping>(
+                  ty, tx, tileYstart, tileXstart, mapping.width);
+
+      // TODO: add tile offset to global output
       // printf("setting mapping.at(%d, %d)\n", y, x);
-      mapping.set(y, x, s_tiles[numUnites % 2].get(ty, tx));
+      mapping.set(y, x, globalIdx);
     }
   }
-}
+} // namespace wccl
 
 } // namespace wccl
--- proj_windowccl/src/wccl_experiment_cuda.cu
+++ proj_windowccl/src/wccl_experiment_cuda.cu
@@ -4,7 +4,19 @@
 #include <thrust/host_vector.h>
 
 int main() {
-  constexpr int rows = 4;
+  // ================= Example 1
+  // constexpr int rows = 4;
+  // constexpr int cols = 5;
+  // // clang-format off
+  // const std::vector<uint8_t> input = {
+  //   1, 0, 0, 0, 1,
+  //   0, 1, 0, 1, 0,
+  //   0, 0, 1, 0, 0,
+  //   0, 1, 0, 0, 1,
+  // };
+  // // clang-format on
+  // ================= Example 2
+  constexpr int rows = 8;
   constexpr int cols = 5;
   // clang-format off
   const std::vector<uint8_t> input = {
@@ -12,35 +24,42 @@ int main() {
     0, 1, 0, 1, 0,
     0, 0, 1, 0, 0,
     0, 1, 0, 0, 1,
+    0, 0, 0, 0, 1,
+    0, 1, 0, 1, 0,
+    1, 0, 0, 0, 0,
+    0, 0, 0, 0, 0,
   };
+  // clang-format on
+
+  typedef short2 KernelColRowType;
+  typedef int MappingType;
+
   if (input.size() != rows * cols) {
     throw std::runtime_error("input size is not equal to rows * cols");
   }
-  // clang-format on
   thrust::host_vector<uint8_t> h_input(rows * cols);
   thrust::copy(input.begin(), input.end(), h_input.begin());
   thrust::device_vector<uint8_t> d_input(rows * cols);
   thrust::copy(h_input.begin(), h_input.end(), d_input.begin());
   // Copy back to check?
   thrust::copy(d_input.begin(), d_input.end(), h_input.begin());
-  for (int i = 0; i < h_input.size(); ++i)
-    printf("%d ", h_input[i]);
-  printf("\n");
 
-  thrust::device_vector<int16_t> d_mapping_vec(rows * cols);
+  thrust::device_vector<MappingType> d_mapping_vec(rows * cols);
 
   // Encapsulate
   wccl::DeviceImage<uint8_t> d_image(d_input, rows, cols);
-  wccl::DeviceImage<short> d_mapping(d_mapping_vec, rows, cols);
+  wccl::DeviceImage<MappingType> d_mapping(d_mapping_vec, rows, cols);
 
   // Kernel
   dim3 tpb(32, 4);
-  dim3 bpg(1, 1);
+  dim3 bpg(d_image.width / tpb.x + (d_image.width % tpb.x ? 1 : 0),
+           d_image.height / tpb.y + (d_image.height % tpb.y ? 1 : 0));
   const int2 tileDims = {32, 4};
   const int2 windowDist = {1, 1};
   size_t shmem = tileDims.x * tileDims.y * 2 * sizeof(short);
-  printf("Launching kernel with shmem = %zu\n", shmem);
-  wccl::connect_kernel<short>
+  printf("Launching (%d, %d) blks kernel with shmem = %zu\n", bpg.x, bpg.y,
+         shmem);
+  wccl::connect_kernel<MappingType, KernelColRowType>
       <<<bpg, tpb, shmem>>>(d_image, d_mapping, tileDims, windowDist);
 
   thrust::host_vector<short> h_mapping = d_mapping_vec;
