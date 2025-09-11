b25ab75094872ea042464caa1732faf657cce70f
icyveins7 <icyveins@hotmail.com>
2025-09-11 11:40:38 +0800

    fix many reference related errors and setup initial kernel test
    this does a single pass unite to a second buffer, and looks like it is
    working as intended.
    reference errors are based on the fact that the struct should not be
    passed to the global kernel as a reference or pointer, since it is a
    host side construct that simply contains a copy of the device side
    pointer (this was causing many issues).
    also reworked the .at() to just use .set() and .get() since we can't
    really throw when inside the kernel, so it's better to no-op when out of
    bounds.

--- proj_windowccl/include/wccl_kernels.cuh
+++ proj_windowccl/include/wccl_kernels.cuh
@@ -9,6 +9,11 @@
 
 namespace wccl {
 
+// NOTE: it is important that when using this as inputs to a kernel, a
+// reference/pointer is not used, since it is merely a container for a copy of
+// the pointers and sizes i.e. just a handy substitute for using 4 separate
+// arguments in the kernel inputs. using a reference/pointer will undoubtedly
+// break the kernel.
 template <typename T> struct DeviceImage {
   T *data; // non-owning, but we leave as raw pointer for CUDA stuff
   unsigned int height;
@@ -17,6 +22,10 @@ template <typename T> struct DeviceImage {
   unsigned int xOffset = 0; // at a tile inside kernels
 
   DeviceImage() = default;
+  __device__ DeviceImage(T *_data, int _height, int _width, int _yOffset = 0,
+                         int _xOffset = 0)
+      : data(_data), height(_height), width(_width), yOffset(_yOffset),
+        xOffset(_xOffset) {}
   DeviceImage(thrust::device_vector<T> &_data, int _height, int _width)
       : data(_data.data().get()), height(_height), width(_width) {
     if (data == nullptr) {
@@ -29,13 +38,13 @@ template <typename T> struct DeviceImage {
   /**
    * @brief Returns the number of elements in the image
    */
-  __host__ __device__ size_t size() { return height * width; }
+  __host__ __device__ size_t size() const { return height * width; }
 
   /**
    * @brief Returns the flattened 1D index from the row and column indices.
    */
   __host__ __device__ unsigned int flattenedIndex(unsigned int row,
-                                                  unsigned int col) {
+                                                  unsigned int col) const {
     return row * width + col;
   }
   /**
@@ -43,31 +52,34 @@ template <typename T> struct DeviceImage {
    * indices. This accounts for the offsets of the current tile.
    */
   __host__ __device__ unsigned int globalIndex(unsigned int row,
-                                               unsigned int col) {
+                                               unsigned int col) const {
     return (row + yOffset) * width + (col + xOffset);
   }
 
   /**
-   * @brief Range-checked access via row/col indices.
+   * @brief Range-checked getter via row/col indices.
    */
-  __host__ __device__ T &at(unsigned int row, unsigned int col) {
+  __host__ __device__ T get(const unsigned int row,
+                            const unsigned int col) const {
     if (row >= height || col >= width)
-      return data[flattenedIndex(height, width)];
-    else
       return cuda::std::numeric_limits<T>::max();
+    return data[flattenedIndex(row, col)];
   }
 
   /**
-   * @brief (Const) Range-checked access via row/col indices.
+   * @brief Range-checked setter via row/col indices. No-op if out of bounds.
    */
-  __host__ __device__ const T &at(unsigned int row, unsigned int col) const {
-    return (const T &)at(row, col);
+  __host__ __device__ void set(const unsigned int row, const unsigned int col,
+                               const T val) {
+    if (row < height && col < width)
+      data[flattenedIndex(row, col)] = val;
   }
 };
 
 template <typename T>
-__device__ T find(const DeviceImage<T> &tile, const int row, const int col) {
-  int idx = tile.flattenedIndex(row, col);
+__device__ T find(const DeviceImage<T> &tile, const unsigned int row,
+                  const unsigned int col) {
+  unsigned int idx = tile.flattenedIndex(row, col);
   // Return directly if inactive i.e. -1 -> -1
   if (tile.data[idx] < 0)
     return tile.data[idx];
@@ -85,47 +97,57 @@ __device__ void unite(const DeviceImage<T> &tile_in, DeviceImage<T> &tile_out,
   for (int ty = threadIdx.y; ty < tile_in.height; ty += blockDim.y) {
     for (int tx = threadIdx.x; tx < tile_in.width; tx += blockDim.x) {
       T root = find(tile_in, ty, tx);
-      if (root < 0)
-        continue;
 
-      // Loop over the surrounding window
-      for (int wy = ty - windowDist.y; wy <= ty + windowDist.y; wy++) {
-        for (int wx = tx - windowDist.x; wx <= tx + windowDist.x; wx++) {
-          if (wy >= 0 && wy < tile_in.height && wx >= 0 && wx < tile_in.width) {
-            T windowRoot = find(tile_in, wy, wx);
-            if (windowRoot < 0)
-              continue;
+      if (root >= 0) {
+        // Loop over the surrounding window
+        for (int wy = ty - windowDist.y; wy <= ty + windowDist.y; wy++) {
+          for (int wx = tx - windowDist.x; wx <= tx + windowDist.x; wx++) {
+            if (wy >= 0 && wy < tile_in.height && wx >= 0 &&
+                wx < tile_in.width) {
+              T windowRoot = find(tile_in, wy, wx);
+              if (windowRoot < 0)
+                continue;
 
-            // Take min root
-            root = root < windowRoot ? root : windowRoot;
+              // Take min root
+              root = root < windowRoot ? root : windowRoot;
+            }
           }
         }
       }
 
       // Write to output tile
-      tile_out.at(ty, tx) = root;
+      printf("Wrote root %d to (%d, %d)\n", root, ty, tx);
+      tile_out.set(ty, tx, root);
     }
   }
 }
 
 template <typename T>
-__global__ void connect_kernel(const DeviceImage<uint8_t> &input,
-                               DeviceImage<T> &mapping, const int2 tileDims,
+__global__ void connect_kernel(const DeviceImage<uint8_t> input,
+                               DeviceImage<T> mapping, const int2 tileDims,
                                const int2 windowDist) {
   static_assert(std::is_signed_v<T>, "mapping type T must be signed");
 
   // Define the tile for this block
-  int tileXstart = blockIdx.x * tileDims.x;
-  int tileYstart = blockIdx.y * tileDims.y;
+  const int tileXstart = blockIdx.x * tileDims.x;
+  const int tileYstart = blockIdx.y * tileDims.y;
+  // Tile may not be fully occupied at the ends
+  const int2 blockTileDims{tileXstart + tileDims.x < (int)input.width
+                               ? tileDims.x
+                               : (int)input.width - tileXstart,
+                           tileYstart + tileDims.y < (int)input.height
+                               ? tileDims.y
+                               : (int)input.height - tileYstart};
 
   // Read entire tile and set shared memory values
   SharedMemory<T> smem;
   // Encapsulate shared memory tile for ergonomics
-  DeviceImage<T> s_tile1{smem.getPointer(), tileDims.y, tileDims.x, tileYstart,
-                         tileXstart};
   // We have 2 workspaces
-  DeviceImage<T> s_tile2{&s_tile1.data[s_tile1.size()], tileDims.y, tileDims.x,
-                         tileYstart, tileXstart};
+  DeviceImage<T> s_tiles[2] = {
+      DeviceImage<T>(smem.getPointer(), blockTileDims.y, blockTileDims.x,
+                     tileYstart, tileXstart),
+      DeviceImage<T>(&s_tiles[0].data[s_tiles[0].size()], blockTileDims.y,
+                     blockTileDims.x, tileYstart, tileXstart)};
 
   for (int ty = threadIdx.y; ty < tileDims.y; ty += blockDim.y) {
     // Define global index for reads (row)
@@ -133,16 +155,44 @@ __global__ void connect_kernel(const DeviceImage<uint8_t> &input,
     for (int tx = threadIdx.x; tx < tileDims.x; tx += blockDim.x) {
       // Define global index for reads (col)
       int x = tileXstart + tx;
-      // Global read; if inactive, set to -1, otherwise set to flattened local
-      // index (for now)
-      s_tile1.at(ty, tx) =
-          input.at(y, x) > 0 ? s_tile1.flattenedIndex(ty, tx) : -1;
+
+      // Global read; if inactive (or out of bounds), set to -1, otherwise
+      // set to flattened local index (for now)
+      if (y < (int)input.height && x < (int)input.width) {
+        const uint8_t site = input.get(y, x);
+        const T initVal = site > 0 ? s_tiles[0].flattenedIndex(ty, tx) : -1;
+
+        printf("tile(%d,%d)<-input.get(%d,%d): %d\n", ty, tx, y, x, initVal);
+        s_tiles[0].set(ty, tx, initVal);
+      } else {
+        // printf("Out of bounds at (%d, %d) in s_tiles[0]\n", y, x);
+        s_tiles[0].set(ty, tx, -1);
+      }
     }
   }
   __syncthreads();
 
-  // Perform first unites
-  unite(s_tile1, s_tile2, windowDist);
+  // Perform unites
+  const int numUnites = 1;
+  for (int i = 0; i < numUnites; ++i) {
+    unite(s_tiles[i % 2], s_tiles[(i + 1) % 2], windowDist);
+    __syncthreads();
+  }
+
+  // TODO: for now, just write this out to global
+  for (int ty = threadIdx.y; ty < tileDims.y; ty += blockDim.y) {
+    int y = tileYstart + ty;
+    if (y >= (int)mapping.height)
+      continue;
+    for (int tx = threadIdx.x; tx < tileDims.x; tx += blockDim.x) {
+      int x = tileXstart + tx;
+      if (x >= (int)mapping.width)
+        continue;
+
+      // printf("setting mapping.at(%d, %d)\n", y, x);
+      mapping.set(y, x, s_tiles[numUnites % 2].get(ty, tx));
+    }
+  }
 }
 
 } // namespace wccl
--- proj_windowccl/src/wccl_experiment_cuda.cu
+++ proj_windowccl/src/wccl_experiment_cuda.cu
@@ -1,3 +1,58 @@
 #include "wccl_kernels.cuh"
 
-int main() { return 0; }
+#include <thrust/device_vector.h>
+#include <thrust/host_vector.h>
+
+int main() {
+  constexpr int rows = 4;
+  constexpr int cols = 5;
+  // clang-format off
+  const std::vector<uint8_t> input = {
+    1, 0, 0, 0, 1,
+    0, 1, 0, 1, 0,
+    0, 0, 1, 0, 0,
+    0, 1, 0, 0, 1,
+  };
+  if (input.size() != rows * cols) {
+    throw std::runtime_error("input size is not equal to rows * cols");
+  }
+  // clang-format on
+  thrust::host_vector<uint8_t> h_input(rows * cols);
+  thrust::copy(input.begin(), input.end(), h_input.begin());
+  thrust::device_vector<uint8_t> d_input(rows * cols);
+  thrust::copy(h_input.begin(), h_input.end(), d_input.begin());
+  // Copy back to check?
+  thrust::copy(d_input.begin(), d_input.end(), h_input.begin());
+  for (int i = 0; i < h_input.size(); ++i)
+    printf("%d ", h_input[i]);
+  printf("\n");
+
+  thrust::device_vector<int16_t> d_mapping_vec(rows * cols);
+
+  // Encapsulate
+  wccl::DeviceImage<uint8_t> d_image(d_input, rows, cols);
+  wccl::DeviceImage<short> d_mapping(d_mapping_vec, rows, cols);
+
+  // Kernel
+  dim3 tpb(32, 4);
+  dim3 bpg(1, 1);
+  const int2 tileDims = {32, 4};
+  const int2 windowDist = {1, 1};
+  size_t shmem = tileDims.x * tileDims.y * 2 * sizeof(short);
+  printf("Launching kernel with shmem = %zu\n", shmem);
+  wccl::connect_kernel<short>
+      <<<bpg, tpb, shmem>>>(d_image, d_mapping, tileDims, windowDist);
+
+  thrust::host_vector<short> h_mapping = d_mapping_vec;
+  for (int r = 0; r < rows; r++) {
+    for (int c = 0; c < cols; c++) {
+      if (h_mapping[r * cols + c] < 0)
+        printf("-- ");
+      else
+        printf("%2d ", h_mapping[r * cols + c]);
+    }
+    printf("\n");
+  }
+
+  return 0;
+}
