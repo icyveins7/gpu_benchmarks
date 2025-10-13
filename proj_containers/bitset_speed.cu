1111084d55886cef332a652a91e48a35f8342601
icyveins7 <icyveins@hotmail.com>
2025-10-13 14:42:15 +0800

    make bitset vectors random to validate bitwise op timings
    they are indeed extremely fast. typical bitwise op like AND or OR take
    2-3 us for the entire 8192*1024 bits (262144 elements of uint32s)

--- proj_containers/bitset_speed.cu
+++ proj_containers/bitset_speed.cu
@@ -1,23 +1,44 @@
 #include "containers.cuh"
 #include "timer.h"
+#include <cstdlib>
 #include <nvtx3/nvToolsExt.h>
 #include <thrust/device_vector.h>
 #include <thrust/host_vector.h>
 
 int main() {
   size_t len = 8192 * 1024;
+
+  // Create 1st vector
+  std::vector<unsigned int> h_bitsetarrayvec(
+      containers::BitsetArray<unsigned int, int>::numElementsRequiredFor(len));
+  for (size_t i = 0; i < h_bitsetarrayvec.size(); ++i)
+    h_bitsetarrayvec[i] = std::rand();
+
   thrust::device_vector<unsigned int> d_bitsetarrayvec(
       containers::BitsetArray<unsigned int, int>::numElementsRequiredFor(len));
+  thrust::copy(h_bitsetarrayvec.begin(), h_bitsetarrayvec.end(),
+               d_bitsetarrayvec.begin());
   containers::BitsetArray<unsigned int, int> d_bitsetarray(
       (containers::Bitset<unsigned int, int> *)d_bitsetarrayvec.data().get(),
       len);
 
+  // Create 2nd vector
+  std::vector<unsigned int> h_bitsetarrayvec2(
+      containers::BitsetArray<unsigned int, int>::numElementsRequiredFor(len));
+  for (size_t i = 0; i < h_bitsetarrayvec2.size(); ++i)
+    h_bitsetarrayvec2[i] = std::rand();
+
   thrust::device_vector<unsigned int> d_bitsetarrayvec2(
       containers::BitsetArray<unsigned int, int>::numElementsRequiredFor(len));
+  thrust::copy(h_bitsetarrayvec2.begin(), h_bitsetarrayvec2.end(),
+               d_bitsetarrayvec2.begin());
   containers::BitsetArray<unsigned int, int> d_bitsetarray2(
       (containers::Bitset<unsigned int, int> *)d_bitsetarrayvec2.data().get(),
       len);
-  thrust::fill(d_bitsetarrayvec2.begin(), d_bitsetarrayvec2.end(), 1);
+
+  // Create 3rd vector
+  thrust::device_vector<unsigned int> d_bitsetarrayvec3(
+      containers::BitsetArray<unsigned int, int>::numElementsRequiredFor(len));
 
   printf("Total data elements = %d\n", d_bitsetarray.numDataElements);
 
@@ -27,8 +48,7 @@ int main() {
   thrust::host_vector<int> h_minIdx(1);
 
   for (int i = 0; i < 3; ++i) {
-
-    d_bitsetarrayvec[0] = 1;
+    // reset min index
     d_minIdx[0] = len;
     int TPB = 128;
     int NUM_BLKS = 64; //(d_bitsetarrayvec.size() + TPB - 1) / TPB;
@@ -38,8 +58,7 @@ int main() {
     h_minIdx = d_minIdx;
     printf("%d\n", h_minIdx[0]);
 
-    d_bitsetarrayvec[0] = 0;
-    d_bitsetarrayvec[d_bitsetarrayvec.size() - 1] = 1;
+    // reset min index
     d_minIdx[0] = len;
     d_floor[0] = len - 1000; // setting and using this makes it very fast
     containers::argminBitGlobalKernel<<<NUM_BLKS, TPB>>>(
@@ -63,12 +82,16 @@ int main() {
     // Test some bitwise operations
     {
       thrust::transform(d_bitsetarrayvec.begin(), d_bitsetarrayvec.end(),
-                        d_bitsetarrayvec2.begin(), d_bitsetarrayvec.begin(),
+                        d_bitsetarrayvec2.begin(), d_bitsetarrayvec3.begin(),
                         thrust::bit_or<unsigned int>());
 
       thrust::transform(d_bitsetarrayvec.begin(), d_bitsetarrayvec.end(),
-                        d_bitsetarrayvec2.begin(), d_bitsetarrayvec.begin(),
+                        d_bitsetarrayvec2.begin(), d_bitsetarrayvec3.begin(),
                         thrust::bit_and<unsigned int>());
+
+      thrust::host_vector<unsigned int> h_check = d_bitsetarrayvec3;
+      printf("%X & %X = %X\n", h_bitsetarrayvec[0], h_bitsetarrayvec2[0],
+             h_check[0]);
     }
   }
 
