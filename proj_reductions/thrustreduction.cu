82ee0ce78e213a9b8a03d14451c69d619dc3aced
icyveins7 <icyveins@hotmail.com>
2025-11-12 17:50:20 +0800

    add thrust reduction via indices directly

--- proj_reductions/thrustreduction.cu
+++ proj_reductions/thrustreduction.cu
@@ -2,6 +2,7 @@
 #include "thrust/host_vector.h"
 #include "thrust/iterator/discard_iterator.h"
 #include "thrust/reduce.h"
+#include <cuda/std/cmath>
 #include <cuda/std/functional> // for cuda::std::equal_to
 #include <cuda_runtime.h>
 #include <iostream>
@@ -52,6 +53,50 @@ template <typename Tkey, typename Tcoord, typename Tval> struct KCVReducer {
   }
 };
 
+template <typename Tkey, typename Tcoord, typename Tval, typename Tidx>
+struct KCVIdxReduction {
+  Tcoord i_min;
+  Tcoord i_max;
+  Tcoord j_min;
+  Tcoord j_max;
+  Tidx idx;
+  Tval val;
+
+  __host__ __device__ Tval magnVal() const { return cuda::std::abs(val); }
+
+  template <typename Tuple>
+  __host__ __device__ KCVIdxReduction operator()(const Tuple &tuple) const {
+    Tidx idx = thrust::get<0>(tuple);
+    KeyCoordValue<Tkey, Tcoord, Tval> kv = thrust::get<1>(tuple);
+    return KCVIdxReduction{kv.i, kv.i, kv.j, kv.j, idx, kv.val};
+  }
+};
+
+template <typename Tkey, typename Tcoord, typename Tval, typename Tidx>
+struct KCVIdxReducer {
+  __host__ __device__ KCVIdxReduction<Tkey, Tcoord, Tval, Tidx>
+  operator()(const KCVIdxReduction<Tkey, Tcoord, Tval, Tidx> &x,
+             const KCVIdxReduction<Tkey, Tcoord, Tval, Tidx> &y) const {
+    Tidx idx;
+    Tval xvalmagn = x.magnVal(), yvalmagn = y.magnVal();
+    Tval val;
+    if (xvalmagn > yvalmagn) {
+      idx = x.idx;
+      val = x.val;
+    } else {
+      idx = y.idx;
+      val = y.val;
+    }
+    return KCVIdxReduction<Tkey, Tcoord, Tval, Tidx>{
+        thrust::min(x.i_min, y.i_min),
+        thrust::max(x.i_max, y.i_max),
+        thrust::min(x.j_min, y.j_min),
+        thrust::max(x.j_max, y.j_max),
+        idx,
+        val};
+  }
+};
+
 int main() {
   printf("Thrust reduce examples\n");
 
@@ -72,6 +117,7 @@ int main() {
   thrust::device_vector<KeyCoordValue<int, int, int>> d_x(10);
   d_x = h_x;
 
+  // =========== Test 1, reduce to find min/max intensity =========
   thrust::device_vector<KCVReduction<int, int, int>> d_y(10);
   thrust::reduce_by_key(
       thrust::make_transform_iterator(d_x.begin(),
@@ -94,6 +140,35 @@ int main() {
            "maxVal: %d\n",
            i, h_y[i].i_min, h_y[i].i_max, h_y[i].j_min, h_y[i].j_max,
            h_y[i].minVal, h_y[i].maxVal);
+  }
+
+  // =========== Test 2, reduce to find representative (signed max magnitude)
+  // intensity =========
+  thrust::device_vector<KCVIdxReduction<int, int, int, int>> d_z(10);
+  thrust::reduce_by_key(
+      thrust::make_transform_iterator(d_x.begin(),
+                                      KeyCoordValue<int, int, int>()),
+      thrust::make_transform_iterator(d_x.end(),
+                                      KeyCoordValue<int, int, int>()),
+      thrust::make_transform_iterator(
+          thrust::make_zip_iterator(
+              thrust::make_counting_iterator(0),
+              d_x.begin()), // zip an incrementing index with it
+          KCVIdxReduction<int, int, int, int>()), // turn the struct into the
+                                                  // end result
+      thrust::make_discard_iterator(),            // discard the 'keys'
+      d_z.begin(),                                // output 'structs'
+      cuda::std::equal_to<int>(),                 // keys are just POD values
+      KCVIdxReducer<int, int, int, int>()         // custom functor to reduce
+  );
+
+  thrust::host_vector<KCVIdxReduction<int, int, int, int>> h_z = d_z;
+
+  for (size_t i = 0; i < h_z.size(); ++i) {
+    printf("[%2zu] // i_min: %d, i_max: %d, j_min: %d, j_max: %d, idx: %d, "
+           "val: %d\n",
+           i, h_z[i].i_min, h_z[i].i_max, h_z[i].j_min, h_z[i].j_max,
+           h_z[i].idx, h_z[i].val);
   }
 
   return 0;
