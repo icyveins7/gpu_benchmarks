e5f54818d2317b77538b89fc51c7f5094b8a91e2
icyveins7 <icyveins@hotmail.com>
2025-11-23 16:00:44 +0800

    attempt to nullptr all initial cub calls to see if it works

--- proj_basicops/run_length_encode.cu
+++ proj_basicops/run_length_encode.cu
@@ -8,7 +8,7 @@
 int main() {
   printf("Run length encoding\n");
 
-  thrust::host_vector<int> h_keys = {0,0,3,3,3,4,4,6,7,7};
+  thrust::host_vector<int> h_keys = {0, 0, 3, 3, 3, 4, 4, 6, 7, 7};
   thrust::device_vector<int> d_keys = h_keys;
 
   thrust::device_vector<int> d_unique_out(10);
@@ -16,17 +16,22 @@ int main() {
   thrust::device_vector<int> d_num_runs_out(1);
 
   size_t temp_storage_bytes = 0;
-  cub::DeviceRunLengthEncode::Encode(nullptr, temp_storage_bytes, d_keys.data().get(),
-                               d_unique_out.data().get(), d_counts_out.data().get(),
-                               d_num_runs_out.data().get(), d_keys.size());
+  cub::DeviceRunLengthEncode::Encode(
+      nullptr, temp_storage_bytes,
+      (int *)nullptr, // d_keys.data().get(),
+      (int *)nullptr,
+      (int *)nullptr, // d_unique_out.data().get(), d_counts_out.data().get(),
+      (int *)nullptr, // d_num_runs_out.data().get(),
+      d_keys.size());
   printf("temp_storage_bytes = %zu\n", temp_storage_bytes);
 
   thrust::device_vector<char> d_temp_storage(temp_storage_bytes);
 
-  for (int iter = 0; iter < 3; ++iter){
-    cub::DeviceRunLengthEncode::Encode(d_temp_storage.data().get(), temp_storage_bytes, d_keys.data().get(),
-                                       d_unique_out.data().get(), d_counts_out.data().get(),
-                                       d_num_runs_out.data().get(), d_keys.size());
+  for (int iter = 0; iter < 3; ++iter) {
+    cub::DeviceRunLengthEncode::Encode(
+        d_temp_storage.data().get(), temp_storage_bytes, d_keys.data().get(),
+        d_unique_out.data().get(), d_counts_out.data().get(),
+        d_num_runs_out.data().get(), d_keys.size());
   }
 
   thrust::host_vector<int> h_unique_out = d_unique_out;
@@ -44,7 +49,6 @@ int main() {
   }
   printf("\n");
   printf("h_num_runs_out: %d\n", h_num_runs_out[0]);
-
 
   return 0;
 }
