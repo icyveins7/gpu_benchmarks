8d245cc8220e1e21f13fe48b8ddf707f615d61c3
icyveins7 <icyveins@hotmail.com>
2025-12-10 15:16:32 +0800

    get within <2x of the cub::DeviceSelect::If by implementing decoupled lookback with 1 thread
    i think if i did it via a warp, it might be faster, along with maybe
    vectorised loads/stores.

--- proj_containers/deviceselect.cu
+++ proj_containers/deviceselect.cu
@@ -1,5 +1,6 @@
 #include "containers/cubwrappers.cuh"
 #include "sharedmem.cuh"
+#include <cooperative_groups.h>
 
 #include <cuda_runtime.h>
 #include <iostream>
@@ -8,6 +9,8 @@
 #include <thrust/device_vector.h>
 #include <thrust/fill.h>
 #include <thrust/host_vector.h>
+
+namespace cg = cooperative_groups;
 
 // Compile-time toggle to use giant struct for tests
 #define USE_BIG_DATA
@@ -57,14 +60,15 @@ __global__ void NaiveSelectIf(const T *data, T *output,
 }
 
 template <typename T, typename SelectOp, int ThreadsPerBlk>
-__global__ void SelectIfInplaceKernel(T *data, int *d_block_offsets,
+__global__ void SelectIfInplaceKernel(T *data, volatile int *d_block_offsets,
+                                      volatile int *d_block_prefixes,
                                       unsigned int *d_num_selected,
                                       unsigned int initialLen,
                                       const SelectOp select_op) {
 
   __shared__ T s_data[ThreadsPerBlk];
   __shared__ unsigned int s_block_count;
-  __shared__ unsigned int s_block_offset;
+  __shared__ unsigned int s_block_prefix;
   if (threadIdx.x == 0)
     s_block_count = 0;
   __syncthreads();
@@ -90,30 +94,92 @@ __global__ void SelectIfInplaceKernel(T *data, int *d_block_offsets,
       s_data[atomicAdd(&s_block_count, 1)] = threadData;
     }
   }
+  __syncthreads();
 
+  // Thread 0: Populate the block offset, but not the prefix yet
+  // We arrange the offsetprefixes in reverse i.e. block 0 writes to the last
+  // element This makes it easier to 'look back' as it would be equivalent to
+  // incrementing the index
+  unsigned int thisBlockOffsetPrefixesIndex = gridDim.x - 1 - blockIdx.x;
   if (threadIdx.x == 0) {
-    s_block_offset = 0; // default is 0 i.e. block 0
-    if (blockIdx.x > 0) {
-      // Spin wait on previous block
-      while (__ldcg(&d_block_offsets[blockIdx.x - 1]) < 0) {
-        __threadfence();
-      }
-      // Update
-      s_block_offset = d_block_offsets[blockIdx.x - 1];
+    // We have the inclusive sum for block 0 already
+    if (blockIdx.x == 0) {
+      d_block_prefixes[thisBlockOffsetPrefixesIndex] = (int)s_block_count;
     }
+    __threadfence(); // prefix should be written first
+    d_block_offsets[thisBlockOffsetPrefixesIndex] = (int)s_block_count;
+    // Programming guide suggests just marking it volatile means i don't need a
+    // special builtin like __stcs:
+    // https://docs.nvidia.com/cuda/cuda-c-programming-guide/#memory-fence-functions
+
+    // Update the global sum
+    atomicAdd(d_num_selected, s_block_count);
   }
-  __syncthreads(); // need all threads to see the block offset
+
+  // First thread: decoupled lookback
+  int rollingPrefix = 0;
+  if ((threadIdx.x == 0) && (blockIdx.x != 0)) {
+    bool complete = false;
+    int blkIdx = thisBlockOffsetPrefixesIndex + 1;
+    while (!complete) {
+      int prevBlkPrefix = d_block_prefixes[blkIdx];
+      if (prevBlkPrefix >= 0) {
+        // Block has completed its own prefix
+        complete = true;
+        rollingPrefix += prevBlkPrefix;
+        break;
+      }
+
+      int prevBlkOffset = d_block_offsets[blkIdx];
+      if (prevBlkOffset < 0) {
+        // Block has not completed its own offset yet
+        __threadfence();
+        // We repeat on the same block
+        continue;
+      }
+      // Otherwise we increment
+      rollingPrefix += prevBlkOffset;
+
+      // Otherwise we look back further
+      if (blkIdx < (int)gridDim.x - 1)
+        blkIdx++;
+    }
+    s_block_prefix = rollingPrefix;
+    // printf("Block %d prefix should be %d\n", blockIdx.x, s_block_prefix);
+    // Update the block prefixes for next blocks
+    d_block_prefixes[thisBlockOffsetPrefixesIndex] =
+        rollingPrefix + s_block_count;
+  }
+  __syncthreads(); // wait for all threads to know where to write
+
+  // // First warp: decoupled lookback
+  // if (threadIdx.x < 32) {
+  //   if (blockIdx.x > 0) {
+  //     bool complete = false;
+  //     while (!complete) {
+  //       // Lookback until we hit something with no block offset
+  //       // NOTE: now to look back, we just go forwards
+  //       for (int i = threadIdx.x;
+  //            i < (int)gridDim.x - (int)thisBlockOffsetPrefixesIndex - 1;
+  //            i += 32) {
+  //         // First thread on first loop should read the +1 block, and so on
+  //         int prevBlkOffset =
+  //             (int)d_block_offsets[i + 1 + thisBlockOffsetPrefixesIndex];
+  //         bool prevBlkOffsetWritten = (prevBlkOffset >= 0);
+  //         bool allOffsetsFound = false;
+  //         if (prevBlkOffsetWritten) {
+  //           cg::coalesced_group g = cg::coalesced_threads();
+  //           g.size()
+  //         }
+  //         // __threadfence(); // maybe i dont need this any more?
+  //       }
+  //     }
+  //   }
+  // }
 
   // Write data for each block
   if (threadIdx.x < s_block_count) {
-    data[s_block_offset + threadIdx.x] = s_data[threadIdx.x];
-  }
-
-  // Update the next block
-  if (threadIdx.x == 0) {
-    __stcs(&d_block_offsets[blockIdx.x], s_block_offset + s_block_count);
-    // and the total count
-    atomicAdd(d_num_selected, s_block_count);
+    data[s_block_prefix + threadIdx.x] = s_data[threadIdx.x];
   }
 }
 
@@ -253,8 +319,10 @@ int main(int argc, char **argv) {
     d_num_selected[0] = 0;
     d_data = h_data;
     thrust::device_vector<int> d_block_offsets(blks, -1);
+    thrust::device_vector<int> d_block_prefixes(blks, -1);
     SelectIfInplaceKernel<DataT, decltype(functor), tpb>
         <<<blks, tpb>>>(d_data.data().get(), d_block_offsets.data().get(),
+                        d_block_prefixes.data().get(),
                         d_num_selected.data().get(), length, functor);
   }
   h_num_selected = d_num_selected;
@@ -267,6 +335,8 @@ int main(int argc, char **argv) {
     if (h_out[i] >= functor.value) {
 #endif
       std::cout << "Error at index " << i << std::endl;
+      std::cout << "Stopping here..." << std::endl;
+      break;
     }
     if (length <= 64) {
 #ifdef USE_BIG_DATA
