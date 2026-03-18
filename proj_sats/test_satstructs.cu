ebc3c6303f1305609c90f51523721fe2b55e7cc4
icyveins7 <icyveins@hotmail.com>
2026-03-18 13:23:41 +0800

    fix test_satstructs

--- proj_sats/test_satstructs.cu
+++ proj_sats/test_satstructs.cu
@@ -35,19 +35,19 @@ TEST(SectionConstruction_PrefixRowsWithSAT, 3) {
   3 3 3 3 3 3 3
   X 4 4 4 4 4 X
   X 4 4 4 4 4 X
-  X X X 1 X X X
+  X X X 5 X X X
   */
 
-  EXPECT_EQ(h_sections[0].startRow, 0);
-  EXPECT_EQ(h_sections[0].endRow, 0);
+  EXPECT_EQ(h_sections[0].startRow, -3);
+  EXPECT_EQ(h_sections[0].endRow, -3);
   EXPECT_EQ(h_sections[0].colOffset, 0);
 
-  EXPECT_EQ(h_sections[1].startRow, 1);
-  EXPECT_EQ(h_sections[1].endRow, 2);
+  EXPECT_EQ(h_sections[1].startRow, -2);
+  EXPECT_EQ(h_sections[1].endRow, -1);
   EXPECT_EQ(h_sections[1].colOffset, 2);
 
-  EXPECT_EQ(h_sections[2].startRow, 3);
-  EXPECT_EQ(h_sections[2].endRow, 3);
+  EXPECT_EQ(h_sections[2].startRow, 0);
+  EXPECT_EQ(h_sections[2].endRow, 0);
   EXPECT_EQ(h_sections[2].colOffset, 3);
 
   EXPECT_EQ(numSections, 3);
@@ -70,23 +70,23 @@ TEST(SectionConstruction_PrefixRowsWithSAT, 4) {
   X 5 5 5 5 5 5 5 X
   X 5 5 5 5 5 5 5 X
   X X 6 6 6 6 6 X X
-  X X X X 1 X X X X
+  X X X X 7 X X X X
   */
 
-  EXPECT_EQ(h_sections[0].startRow, 0);
-  EXPECT_EQ(h_sections[0].endRow, 0);
+  EXPECT_EQ(h_sections[0].startRow, -4);
+  EXPECT_EQ(h_sections[0].endRow, -4);
   EXPECT_EQ(h_sections[0].colOffset, 0);
 
-  EXPECT_EQ(h_sections[1].startRow, 1);
-  EXPECT_EQ(h_sections[1].endRow, 1);
+  EXPECT_EQ(h_sections[1].startRow, -3);
+  EXPECT_EQ(h_sections[1].endRow, -3);
   EXPECT_EQ(h_sections[1].colOffset, 2);
 
-  EXPECT_EQ(h_sections[2].startRow, 2);
-  EXPECT_EQ(h_sections[2].endRow, 3);
+  EXPECT_EQ(h_sections[2].startRow, -2);
+  EXPECT_EQ(h_sections[2].endRow, -1);
   EXPECT_EQ(h_sections[2].colOffset, 3);
 
-  EXPECT_EQ(h_sections[3].startRow, 4);
-  EXPECT_EQ(h_sections[3].endRow, 4);
+  EXPECT_EQ(h_sections[3].startRow, 0);
+  EXPECT_EQ(h_sections[3].endRow, 0);
   EXPECT_EQ(h_sections[3].colOffset, 4);
 
   EXPECT_EQ(numSections, 4);
