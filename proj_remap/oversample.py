8ea503bf373619d7ee1e4ef6c7a42dd89115f655
icyveins7 <icyveins@hotmail.com>
2026-04-23 13:56:01 +0800

    add rotation to the oversample script

--- proj_remap/oversample.py
+++ proj_remap/oversample.py
@@ -1,10 +1,12 @@
 import numpy as np
 
 from scipy.interpolate import RegularGridInterpolator
+from scipy.spatial.transform import Rotation
 
-factor = 5
+factor = 3
 padding = factor // 2
 step = 0.8/factor
+angle = np.pi/2 # corresponds to -90 deg in the oversample_remap.cu option
 
 inwidth = 4
 inheight = 4
@@ -21,16 +23,34 @@ ypad = np.arange(y.size + 2*padding) - padding
 print(xpad)
 print(ypad)
 
-interp = RegularGridInterpolator((xpad, ypad), zpad)
+interp = RegularGridInterpolator((xpad, ypad), zpad, bounds_error=False, fill_value=None) # pyright:ignore
 
 outShape = np.array([outheight, outwidth])
 xp = np.arange(factor * outShape[1]) * step - step * padding
 yp = np.arange(factor * outShape[0]) * step - step * padding
 print(xp)
 print(yp)
-xg, yg = np.meshgrid(xp, yp, indexing="ij")
-# zp = interp(np.vstack((xg.ravel(), yg.ravel())).T).reshape((yp.size,xp.size)).T
-zp = interp((xg, yg))
+# xpg_0, ypg_0 = np.meshgrid(xp, yp, indexing="ij")
+xpg_0, ypg_0 = np.meshgrid(xp, yp)
+
+centre = np.array([
+    inwidth // 2 - (0.5 * (inwidth%2==0)),
+    inheight // 2 - (0.5 * (inheight%2==0))
+])
+
+if angle != 0:
+    rotmat = Rotation.from_rotvec(angle * np.array([0, 0, 1])).as_matrix()[:2,:2]
+    print(rotmat)
+    xypg = np.vstack((xpg_0.ravel(), ypg_0.ravel())) - centre.reshape((-1,1)) # from centre
+    xypg = rotmat @ xypg
+    xpg = xypg[0].reshape(xpg_0.shape) + centre[0] # add back
+    ypg = xypg[1].reshape(ypg_0.shape) + centre[1]
+else:
+    xpg = xpg_0
+    ypg = ypg_0
+
+
+zp = interp((xpg, ypg)).T
 
 
 zfinal = np.zeros((outShape[0], outShape[1]), np.float64)
