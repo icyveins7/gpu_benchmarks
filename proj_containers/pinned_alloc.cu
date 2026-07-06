a9b432c575f2ec780c7e2c436ffe0ea240f0e910
icyveins7 <icyveins@hotmail.com>
2026-07-06 10:19:14 +0800

    add explicit timer and comments for pinned alloc test
    note that resizing down doesn't trigger a realloc, reallocing up
    triggers a zero-ing kernel (both as std::vector would)

--- proj_containers/pinned_alloc.cu
+++ proj_containers/pinned_alloc.cu
@@ -1,28 +1,41 @@
+#include "timer.h"
 #include <iostream>
 
 #include "pinnedalloc.cuh"
 
 #include <nvtx3/nvToolsExt.h>
 
-int main() {
+int main(int argc, char* argv[]) {
   printf("Comparison of pinned host allocations\n");
 
   size_t len = 10;
+  if (argc > 1) {
+    len = atoi(argv[1]);
+  }
+  printf("Using length %zu\n", len);
+
+  HighResolutionTimer<> timer;
 
   for (int iter = 0; iter < 3; ++iter) {
     nvtxRangePush("cudaMallocHost");
-    int *rp;
+    int* rp;
+
+    timer.start();
 
     // nsys profile says this calls cudaHostAlloc instead;
     // may be due to C++ overloads, but anyway it should be the same thing
     cudaMallocHost(&rp, len * sizeof(int));
 
+    timer.stop();
+
     for (size_t i = 0; i < len; ++i) {
       rp[i] = i;
     }
 
-    for (size_t i = 0; i < len; ++i) {
-      printf("%d\n", rp[i]);
+    if (len < 64) {
+      for (size_t i = 0; i < len; ++i) {
+        printf("%d\n", rp[i]);
+      }
     }
 
     cudaFreeHost(rp);
@@ -34,13 +47,29 @@ int main() {
   // time required for cudaMallocHost instead.
   for (int iter = 0; iter < 3; ++iter) {
     nvtxRangePush("thrust_pinned");
+
+    timer.start();
     thrust::pinned_host_vector<int> tp(len);
-    for (size_t i = 0; i < len; ++i) {
+    timer.stop();
+
+    // does resizing lower create an unnecessary realloc?
+    tp.resize((size_t)(len * 0.9));
+
+    for (size_t i = 0; i < tp.size(); ++i) {
       tp[i] = i;
     }
 
-    for (size_t i = 0; i < len; ++i) {
-      printf("%d\n", tp[i]);
+    // what about if i resize back to the original?
+    // NOTE: so this works exactly the same as std::vector,
+    // in that it tries to zero the 'new' elements
+    // hence it launches a kernel to do this, which is accompanied
+    // by a cudaStreamSynchronize
+    tp.resize(len);
+
+    if (len < 64) {
+      for (size_t i = 0; i < tp.size(); ++i) {
+        printf("%d\n", tp[i]);
+      }
     }
     nvtxRangePop();
   }
